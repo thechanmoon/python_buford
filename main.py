74bbf001692de4ef82466908e854f594135764c6
Chan Park <thechanmoon@gmail.com>
2023-06-25 11:35:37 -0400

    make it better

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from requests import get
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 base_url = "https://weworkremotely.com/remote-jobs/search?search_uuid=&term="
 search_term = "python"
 option = "&button=&sort=any_time"
@@ -8,12 +11,14 @@ if response.status_code != 200:
     print("Error")
 else:
     soup = BeautifulSoup(response.text, "html.parser")
+    # print(soup.find_all('title'))
     job_sections = soup.find_all('section', class_='jobs')
-    for job_section in job_sections:
-        job_posts = job_section.find_all('li')
-        job_posts.pop(-1)
-        for job_post in job_posts:
-            print(job_post)
- 
+    print(soup.find_all('section', class_='jobs'))
+    # for job_section in job_sections:
+    #     job_posts = job_section.find_all('li')
+    #     job_posts.pop(-1)
+    #     for job_post in job_posts:
+    #         print(job_post)
+
 # pkgs.chromium
 # pkgs.chromedriver
