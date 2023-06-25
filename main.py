8e1d61268570e74eff77ef429cb3296e4b19f693
Chan Park <thechanmoon@gmail.com>
2023-06-25 17:24:52 -0400

    make it better

--- main.py
+++ main.py
@@ -10,15 +10,43 @@ response = get(f"{base_url}{search_term}{option}")
 if response.status_code != 200:
     print("Error")
 else:
+    results = []
     soup = BeautifulSoup(response.text, "html.parser")
     # print(soup.find_all('title'))
     job_sections = soup.find_all('section', class_='jobs')
     print(soup.find_all('section', class_='jobs'))
-    # for job_section in job_sections:
-    #     job_posts = job_section.find_all('li')
-    #     job_posts.pop(-1)
-    #     for job_post in job_posts:
-    #         print(job_post)
+    for job_section in job_sections:
+        job_posts = job_section.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            # print(job_post)
+            anchors = post.find_all('a')
+            anchor = anchors[1]
+            link = anchor['href']
+            # print(anchor)
+            company, kind, region = anchor.find_all('span', class_="company")
+            title = anchor.find('span', class_='title')
+            # print(company.string, kind.string, region.string, title.string)
+            job_data = {
+                'company': company.string,
+                'region': region.string,
+                'postion': title.string
+            }
 
+            results.append(job_data)
+            # print("///////////////")
+            # print("///////////////")
+    print(len(results))
+    # print(results[0])
+    # print(results[1])
+    # print(results[2])
+    # print(results[3])
+
+    for index, result in enumerate(results):
+        print(index + 1, ' - ', result)
 # pkgs.chromium
 # pkgs.chromedriver
+
+
+# for num in range(9):
+#     print(f"2 x {num+1} = {2*(num+1)}")
